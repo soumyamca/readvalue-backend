f452215e6ad669e7faa06c0b8e08c52a584d72e6
soumyamca <soumyamoolamannil@gmail.com>
2022-08-12 17:04:29 +0530

    code updated

--- app.js
+++ app.js
@@ -6,11 +6,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 
 app.use(bodyParser.json())
+app.get('/',(req,res)=>{
+    res.send("welcome to my wesite ")
+})
 
 app.post('/read',(req,res)=>{
     var getName=req.body.name
     var getRoll=req.body.roll
     res.json({"name":getName,"roll":getRoll})
+
+    app.post('/add',(req,res)=>{
+        var getNum1=parseFloat(req.body.num1 )
+        var getNum2=parseFloat(req.body.num2 )
+        var result=getNum1+getNum2
+        res.json({"result":result})
+    })
 
 })
 app.listen(7000,()=>{
